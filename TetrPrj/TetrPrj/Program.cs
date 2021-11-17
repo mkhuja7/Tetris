bd3366ae89051a6841d9130a9fbddec6b5fffb39
mkhuja7 <45531280+mkhuja7@users.noreply.github.com>
2021-11-17 11:59:47 +0500

    add data

--- TetrPrj/TetrPrj/Program.cs
+++ TetrPrj/TetrPrj/Program.cs
@@ -6,7 +6,7 @@ namespace TetrPrj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!");
         }
     }
 }
