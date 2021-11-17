61957bf23adc4c57b57f537ed8d3af495f533113
mkhuja7 <45531280+mkhuja7@users.noreply.github.com>
2021-11-17 11:38:07 +0500

    created hello world

---
+++ TetrPrj/TetrPrj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TetrPrj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
